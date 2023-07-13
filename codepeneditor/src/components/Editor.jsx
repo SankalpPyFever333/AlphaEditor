fe16f7437ead784af168255048a6511d829b05a5
sankalp <sankalppandey696@gmail.com>
2023-07-13 15:35:54 +0530

    building editor component

--- codepeneditor/src/components/Editor.jsx
+++ codepeneditor/src/components/Editor.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import { Box , styled } from '@mui/material'
+
+
 
 function Editor() {
   return (
-    <div>
-      hello Editor
-    </div>
+      // we use <Box> from MUI instead of <div>, it works same as <div>.
+    <Box>
+      <Box>
+            <Box>
+                  {/* making that Box to work as a span. */}
+                  <Box component="span"
+                  style={{
+                        background:"red",
+                        height: 20,
+                        width:20,
+                        display:"flex",
+                        placeContent:"center",
+                        borderRadius:5,
+                        marginBottom:5,
+                        paddingBottom:3 
+                  }}
+                  >/</Box>
+                  HTML
+            </Box>
+      </Box>
+      <Box>
+
+      </Box>
+    </Box>
   )
 }
 
--- codepeneditor/src/components/Header.jsx
+++ codepeneditor/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AppBar, Toolbar ,styled} from '@mui/material';
+import { AppBar, IconButton, Toolbar ,styled} from '@mui/material';
 
 function Header() {
   // changing the css of mui component. We pass the component name in the styled() upon which I want to add css.
@@ -14,9 +14,12 @@ return (
     <div>
       <AppBarContainer>
         <Toolbar>
+          <IconButton>
           <img src= "https://img.icons8.com/?size=512&id=p03EZMcmuZH5&format=png" alt="logo" style={{width:40}} />
+          </IconButton>
         </Toolbar>
       </AppBarContainer>
+
     </div>
   )
 }
