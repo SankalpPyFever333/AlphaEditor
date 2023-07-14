5ac97bc6a9597a89da270f1126ab1dd6fc92929e
sankalp <sankalppandey696@gmail.com>
2023-07-14 12:40:21 +0530

    added react-codemirror for making editor

--- codepeneditor/src/components/Editor.jsx
+++ codepeneditor/src/components/Editor.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import { Box , styled } from '@mui/material'
+import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
+const Heading = styled(Box)`
+  background: #1d1e22;
+  display: flex;
+  padding: 9px 12px;
+`
 
+const Header = styled(Box)`
+  display: flex;
+  background: #060606;
+  color: #AAAEBC;
+  justify-content: space-between;
+  font-weight: 700px;
+
+
+`
+
+// react-codemirror is a react library that will detect the html, css and JS.
 
 
 function Editor() {
   return (
       // we use <Box> from MUI instead of <div>, it works same as <div>.
     <Box>
-      <Box>
-            <Box>
+      <Header>
+            <Heading>
                   {/* making that Box to work as a span. */}
                   <Box component="span"
                   style={{
@@ -23,8 +40,9 @@ function Editor() {
                   }}
                   >/</Box>
                   HTML
-            </Box>
-      </Box>
+            </Heading>
+            <CloseFullscreenIcon/>
+      </Header>
       <Box>
 
       </Box>
